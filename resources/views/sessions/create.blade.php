df05487faa849acd9b099f695e901f9607ad5a3b
JasonG <jasongzj@163.com>
2018-04-15 16:21:58 +0000

    finish login and logout

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="form-group">
                     <label for="password">密码：</label>
-                    <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    <input type="password" name="password" class="form-control" value="">
                 </div>
                 <div class="checkbox">
                     <label><input type="checkbox" name="remember"> 记住我</label>
